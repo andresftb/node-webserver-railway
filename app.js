7d9b6182f4063e60ef8bbf581ac58c6ae45f1c58
Andres Trujillo <andres.trujillo@chiper.co>
2022-12-21 12:42:00 -0500

    webserver 20

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express')
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+const hbs = require('hbs');
+
+
+
+//Handlebars
+app.set('view engine','hbs');
+
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+
+
+
+
+
+//middleware - servir contenido estatico
+
+app.use(express.static('public'));
+
+
+
+app.get('/',  (req, res) => {
+  res.render('home',{
+    nombre: 'Andres T',
+    titulo: 'Curso de node'
+  });
+});
+
+app.get('/generic',  (req, res) => {
+  res.render('generic',{
+    nombre: 'Andres T',
+    titulo: 'Curso de node'
+  });
+});
+
+app.get('/elements',  (req, res) => {
+  res.render('elements',{
+    nombre: 'Andres T',
+    titulo: 'Curso de node'
+  });
+});
+
+
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html')
+  });
+
+  app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html')
+  });
+  
+//app.get('*', (req, res) => {
+    //res.sendFile(__dirname + '/public/404.html')
+//});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
